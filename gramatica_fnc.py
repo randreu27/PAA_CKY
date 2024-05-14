706483dd2354d156e7fec61a6f405faef2a573e2
Arete1OO <104272950+Arete1OO@users.noreply.github.com>
2024-05-14 13:55:40 +0200

    jf

--- gramatica_fnc.py
+++ gramatica_fnc.py
@@ -1,5 +1,5 @@
 class Gramatica_FNC:
-    def __init__(self, raw_text):
+    """def __init__(self, raw_text):
         self.raw = raw_text
         # Llegim el arxiu de text
         text = open(raw_text, 'r')
@@ -10,6 +10,7 @@ class Gramatica_FNC:
         for line in lines:
             aux = line.split()
             splitted.append(aux)
+        print(splitted)
 
         # Eliminem els simbols: -, >, |, ...
         simbols = ['-', '>', '->', '|']
@@ -18,6 +19,27 @@ class Gramatica_FNC:
             for word in line:
                 if word in simbols:
                     aux = line.remove(word)
-            cleaned.append(aux)
-        
-        
+            cleaned.append(aux)"""
+
+    def __init__(self, file):
+        """
+        Input: file (string) amb el nom del fitxer que conté la gramàtica
+        """
+        self.grammar = {}
+        with open(file) as f:
+            for line in f:
+                line = line.split()
+                # el primer element és el símbol no terminal i a partir del segon,
+                # cada dos (obviem el |) són els predicats
+                left, right = line[0], line[2::2]
+                self.grammar[left] = right
+        print(self.grammar)
+
+    def CKY_det(self, tira: str):
+        """
+        Input: tira de caràcters (string)
+        Output: True si la tira de caràcters pertany a la llengua de la gramàtica, False en cas contrari
+        """
+        # for s in range(len(tira)):
+
+Gramatica_FNC("g2.txt")
