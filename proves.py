faf8cf7bb2836a04b1978fb6beba36a4302c355c
randreu27 <ra270404@gmail.com>
2024-05-29 20:24:03 +0200

    LA GRAMATICA 4 PROBABILISTA NO FUNCIONA

--- proves.py
+++ proves.py
@@ -97,12 +97,9 @@ def proves_internes():
         print("Proves de la Gramàtica 'g4.txt':")
         cnf_grammar = Gramatica_FNC('g4.txt', to_fnc=True)
 
-        proves_g4 = ['a', 'aa', 'aaa', 'aaaa', 'aaaaa', 'aaaaaaa', 'b', 'bb', 'bbb', 'bbbb',
-                    'bbbbb', 'ab', 'aab', 'aaab', 'aaaab', 'aaaaaab', 'abab', 'aba', 'abaa',
-                    'abaaa', 'abaab', 'bbbaaa', 'aabaaaa']
+        proves_g4 = ['aaa','bbb', 'aba', 'aababaa', 'aaaaaa', 'baba', 'bbbbbabbbbb', 'a', 'b']
 
-        labels_g4 = [False, False, False, False, False, False, False, False, False, False, False, False,
-                            False, False, False, False, False, False, False, False, False, False, False]
+        labels_g4 = [True, True, True, True, False, False, True, True, True]
 
         predicted_g4 = []
         for elem in proves_g4:
@@ -112,5 +109,30 @@ def proves_internes():
             print("La gramàtica s'ha identificat corectament!")
         else:
             print("La gramàtica NO s'ha identificat corectament")
+
+        print('\n')
+        print('\n')
+        print("Proves de la Gramàtica 'g4_prob.txt':")
+
+        cnf_grammar = Gramatica_FNC('g4_prob.txt', to_fnc=True, pcky=True)
+
+        proves_g4 = ['aaa','bbb', 'aba', 'aababaa', 'aaaaaa', 'baba', 'bbbbbabbbbb', 'a', 'b']
+
+        labels_g4 = [True, True, True, True, False, False, True, True, True]
+
+        predicted_g4 = []
+        for elem in proves_g4:
+            if cnf_grammar.CKY_prob(elem) > 0:
+                predicted_g4.append(True)
+            else:
+                predicted_g4.append(False)
+
+        if predicted_g4 == labels_g4:
+            print("La gramàtica s'ha identificat corectament!")
+        else:
+            print("La gramàtica NO s'ha identificat corectament")
+
+        print(predicted_g4)
+        print(cnf_grammar.probabilities)
 
 proves_internes()
