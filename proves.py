a044502530d57dcf59d52cc5ca1318c3af40a74c
randreu27 <ra270404@gmail.com>
2024-05-31 21:45:23 +0200

    act

--- proves.py
+++ proves.py
@@ -121,15 +121,8 @@ def proves_internes():
 
     labels_g4 = [True, True, True, True, False, False, True, True, True]
 
-        predicted_g4 = []
-        for elem in proves_g4:
-            if cnf_grammar.CKY_prob(elem) > 0:
-                predicted_g4.append(True)
-            else:
-                predicted_g4.append(False)
     predicted_g4 = []
     for elem in proves_g4:
-        print(cnf_grammar.CKY_prob(elem))
         if cnf_grammar.CKY_prob(elem) > 0:
             predicted_g4.append(True)
         else:
@@ -139,9 +132,5 @@ def proves_internes():
         print("La gramàtica s'ha identificat corectament!")
     else:
         print("La gramàtica NO s'ha identificat corectament")
-
-    print(predicted_g4)
-    print(cnf_grammar.probabilities)
-
 
 proves_internes()
